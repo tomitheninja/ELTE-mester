6cc5a36cf8e7ee7e86581d202a76effedab5069e
tomitheninja <suditomi@pm.me>
2019-08-23 19:42:59 +0200

    Solve exercise

---
+++ kozephalado/idojaras/1/main.py
@@ -0,0 +1,31 @@
+
+num_cities, num_measurements = map(int, raw_input().split(" "))
+
+INFINITY = 999
+
+results = []
+
+for i in range(0, num_cities):
+  xs = map(int, raw_input().split())
+
+  sum_of_city = 0
+  min_of_city = +INFINITY
+  max_of_city = -INFINITY
+  for x in xs:
+    sum_of_city += x
+    min_of_city = min(x, min_of_city)
+    max_of_city = max(x, max_of_city)
+
+  avg_of_city = float(sum_of_city) / num_measurements
+
+
+  # # criteria:
+  # #  A=diff(avg, min)
+  # #  B=diff(avg, max)
+  # #  A < B
+  if avg_of_city - min_of_city < max_of_city - avg_of_city:
+    results.append(str(i + 1))
+
+result1 = str(len(results)) # number of results
+result2 = " ".join(results) # the resulst joined by space
+print(result1 + " " + result2)
