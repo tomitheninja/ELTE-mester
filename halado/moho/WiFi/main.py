d20d2ab2a3b66ca29eea01e6fa5a317e80680d11
tomitheninja <suditomi@pm.me>
2019-08-27 20:34:05 +0200

    Optimize halado/moho/WiFi

--- halado/moho/WiFi/main.py
+++ halado/moho/WiFi/main.py
@@ -8,28 +8,29 @@ N, H = list(map(int, input().split(' ')))
 R = list(map(int, input().split(' ')))
 
 # Utils
-def find_gt(x):
-    point_index = 0
+def find_gt(x, start_index = 0):
+    point_index = start_index
     while point_index + 1 < len(R) and R[point_index] <= x:
         point_index += 1
     return point_index
 
-def find_max(x):
-    point_index = 0
+def find_max(x, start_index = 0):
+    point_index = start_index
     while point_index + 1 < len(R) and R[point_index + 1] <= x:
         point_index += 1
     return point_index
 
 results = []
+
 next_uncovered_index = 0
 while True:
-    candidate_index = find_max(R[next_uncovered_index] + H)
+    candidate_index = find_max(R[next_uncovered_index] + H, next_uncovered_index)
     results.append(str(candidate_index + 1))
 
     covered_until_val = R[candidate_index] + H
     if (covered_until_val >= R[-1]):
         break
-    next_uncovered_index = find_gt(covered_until_val)
+    next_uncovered_index = find_gt(covered_until_val, candidate_index)
 
 print(len(results))
 print(" ".join(results))
